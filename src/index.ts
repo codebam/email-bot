1041f05c7496fb49adcef23cac87fee7e0fd536d
Sean Behan <codebam@riseup.net>
2024-12-04 20:32:19 -0500

    email worker

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { EmailMessage } from 'cloudflare:email';
+import { createMimeMessage } from 'mimetext';
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async email(message, env, ctx) {
+		const content = await new Response(message.raw).text();
+		const messages = [
+			{ role: 'system', content: 'you are an email responder, reply to the given message' },
+			{ role: 'user', content },
+		];
+		// @ts-expect-error broken bindings
+		const { response } = await env.AI.run('@cf/meta/llama-3.2-11b-vision-instruct', { messages });
+		const msg = createMimeMessage();
+		msg.setHeader('In-Reply-To', message.headers.get('Message-ID') as string);
+		msg.setSender({ name: 'Sean Behan', addr: 'contact@seanbehan.ca' });
+		msg.setRecipient(message.from);
+		msg.setSubject('Auto-reply');
+		msg.addMessage({
+			contentType: 'text/plain',
+			data: `${response}\n\nThis was an automated message. If this is urgent, message Sean on Telegram https://t.me/codebam`,
+		});
+
+		const replyMessage = new EmailMessage('contact@seanbehan.ca', message.from, msg.asRaw());
+
+		await message.reply(replyMessage);
+		await env.SENDEMAIL.send(replyMessage);
+		await message.forward('codebam@riseup.net');
 	},
 } satisfies ExportedHandler<Env>;
